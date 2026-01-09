9caf8d8153d32fe9a0ff4ca1835c6654330dd232
Hedi <montanohedilberto@gmail.com>
2026-01-08 22:31:24 -0400

    add: Collection counters

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,49 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+const Genre = require('../models/genre');
+const Album = require('../models/album');
+
+router.get('/', async function(req, res, next) {
+  try {
+    const genres = await Genre.find().sort({ name: 1 }).exec();
+
+    const agg = await Album.aggregate([
+      { $unwind: '$genre' },
+      { $group: { _id: '$genre', count: { $sum: 1 } } }
+    ]).exec();
+
+    const countMap = {};
+    agg.forEach((row) => { countMap[row._id.toString()] = row.count; });
+
+    const genreCounts = genres.map((g) => countMap[g._id.toString()] || 0);
+    const genreNames = genres.map((g) => g.name);
+
+    res.render('index', { title: 'Express', genreCounts, genreNames });
+  } catch (err) {
+    return next(err);
+  }
+});
+
+router.get('/api/genre-counts', async function(req, res, next) {
+  try {
+    const genres = await Genre.find().sort({ name: 1 }).exec();
+
+    const agg = await Album.aggregate([
+      { $unwind: '$genre' },
+      { $group: { _id: '$genre', count: { $sum: 1 } } }
+    ]).exec();
+
+    const countMap = {};
+    agg.forEach((row) => { countMap[row._id.toString()] = row.count; });
+
+    const genreCounts = genres.map((g) => countMap[g._id.toString()] || 0);
+    const genreNames = genres.map((g) => g.name);
+
+    res.json({ genreNames, genreCounts });
+  } catch (err) {
+    return next(err);
+  }
 });
 
 module.exports = router;
